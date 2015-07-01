cdf385aee0482c0a09ea17274878594e1e65bb16
Steve Smith <steve.smith@sonoahealth.com>
2015-07-01 16:30:51 +1000

    Selecting multiple answers of a question and adding an action will now allow to add a logic out port that is connected to both

--- app/views/v_logicControlsView.js
+++ app/views/v_logicControlsView.js
@@ -42,10 +42,10 @@ define(
                     //'keyup #questionValue': 'questionUpdate',
                     //'change #questionType': 'changeQuestionTypeDropdown',
                     'change': 'changeHandler',
-                    'click': 'addHandler',
+                    'click': 'addHandler'
                     // rule for add rule...
-                    'click #logic-header-button-add-rule': 'addRule',
-                    'click #logic-header-button-add-action': 'addAction'
+                    //'click #logic-header-button-add-rule': 'addRule',
+                    //'click #logic-header-button-add-action': 'addAction'
                     // rule for add action...
                 },
                 render: function () {
@@ -58,7 +58,7 @@ define(
                 addHandler: function(e)
                 {
 
-                    console.log(' add handler - click ', this.$(e.target).data('action'));
+                    //console.log(' add handler - click ', this.$(e.target).data('action'));
 
                     var logic = window.selectedQuestion.model.get('logic');
 
@@ -86,7 +86,7 @@ define(
 
                                 //templateData.calculationBlocks = calculationBlockCompiled; // Add it as a returnedHTML variable to data (check it gets parsed that way in the .hbs file)
 
-                                // or if we're using olf fashioned square brackets method.
+                                // or if we're using old fashioned square brackets method.
 
                                 var ruleCompiled = HRT.templates['logicRule.hbs'](templateData); // .replace('[[[calculationBlocks]]]', calculationBlockCompiled);
 
@@ -103,52 +103,71 @@ define(
 
                                 window.questionModel.set('ruleAdded', true);
 
+                                $('#logic-header-button-add-action').removeClass('btnDisabled');
+                                $('#logic-header-button-add-action').removeAttr('disabled');
+
                                 break;
 
                             case 'logicAction':
 
-                                if (window.selectedQuestion != null) {
+                                //var ruleNumber = parseInt(this.$(e.target).attr('id').split('logic-header-button-add-calculation-block-rule-')[1]);
 
-                                    var parentLogicWrapper = graph.getCell(window.selectedQuestion.model.get('parent'));
+                                //console.log('rule calc block num', ruleNumber);
 
-                                    var newOutports = parentLogicWrapper.attributes.outPorts;
-                                    var ar = [];
-                                    for (lo in newOutports) {
-                                        ar[lo] = newOutports[lo];
-                                    }
-
-                                    var newOutportName = "out " + (newOutports.length + 1);
-
-                                    ar.push(newOutportName);
-                                    parentLogicWrapper.set('outPorts', ar);
-
-
-                                    // Now we'll work out the links
+                                //if (window.selectedQuestion != null) {
 
                                     // Go from question element
-                                    var elementLogicWrapperID = $('#rule_1_calculationblock_1_operand_answer option:selected').attr('data-parent');
+                                    var elementLogicWrapperID = $('#rule_' + window.selectedRule + '_calculationblock_'+ window.selectedCalculation +'_operand_answer option:selected').attr('data-parent');
 
                                     // To answer element
-                                    var elementAnswerID = $('#rule_1_suffix_answer_value option:selected').attr('data-element');
+                                    var elementAnswerIDArray = [];
 
-                                    console.log(' logic connect id ', elementLogicWrapperID, elementAnswerID );
+                                    $('#rule_'+ window.selectedRule +'_suffix_answer_value option:selected').each(function(a,b){
+
+                                        elementAnswerIDArray.push($(this).attr('data-element'));
+
+                                    });
+
+                                    //console.log(' logic connect id ', elementLogicWrapperID, elementAnswerID );
 
                                     //
-                                    if (elementLogicWrapperID.length > 0  && elementAnswerID.length > 0) {
+                                    if (elementLogicWrapperID && elementAnswerIDArray.length > 0) {
 
-                                        var newLogicOutportAnswerLink = new joint.shapes.devs.Link({
-                                            source: {
-                                                id: elementLogicWrapperID,
-                                                port: newOutportName // This is potentially one of many, so suffix with count of 1
-                                            },
-                                            target: {
-                                                id: elementAnswerID
-                                            }
-                                        });
+                                        var parentLogicWrapper = graph.getCell(window.selectedQuestion.model.get('parent'));
 
-                                        graph.addCells(
-                                            [newLogicOutportAnswerLink]
-                                        );
+                                        var newOutports = parentLogicWrapper.attributes.outPorts;
+                                        var ar = [];
+                                        for (lo in newOutports) {
+                                            ar[lo] = newOutports[lo];
+                                        }
+
+                                        var newOutportName = "out " + (newOutports.length + 1);
+
+                                        ar.push(newOutportName);
+                                        parentLogicWrapper.set('outPorts', ar);
+
+                                        // Now we'll work out the links - and loop over the selected answers.
+
+                                        var newLogicOutportAnswerLink;
+
+                                        for(var selectedElementAnswerID in elementAnswerIDArray) {
+
+                                            newLogicOutportAnswerLink = new joint.shapes.devs.Link({
+                                                source: {
+                                                    id: elementLogicWrapperID,
+                                                    port: newOutportName // This is potentially one of many, so suffix with count of 1
+                                                },
+                                                target: {
+                                                    id: elementAnswerIDArray[selectedElementAnswerID]
+                                                }
+
+                                            });
+
+                                            graph.addCells(
+                                                [newLogicOutportAnswerLink]
+                                            );
+
+                                        }
 
 
                                         //get ports from logicwrapper...
@@ -167,9 +186,16 @@ define(
 
                                     }
 
-                                }
+                                //}
+
+                                // Once a new action is added, we must disable adding action until after another rule is added
+
+                                $('#logic-header-button-add-action').addClass('btnDisabled');
+                                $('#logic-header-button-add-action').attr('disabled', 'disabled');
 
                                 window.questionModel.set('actionAdded', true);
+
+
 
                             break;
 
@@ -177,30 +203,27 @@ define(
                             case 'calculationBlock':
 
 
-                                var ruleNumber = parseInt(this.$(e.target).attr('id').split('logic-header-button-add-calculation-block-rule-')[1]);
-
-                                //console.log('rule calc block num', ruleNumber);
+                                window.selectedRule = parseInt(this.$(e.target).attr('id').split('logic-header-button-add-calculation-block-rule-')[1]);
 
                                  //var templateData;
                                  templateData = window.loadedData;
 
                                  // Add dynamic vars to the template.
-                                 templateData.ruleNum = ruleNumber;
-                                 templateData.ruleSortIndex = ruleNumber;
-                                 templateData.calculationNum = logic.rules[ruleNumber-1]['calculationBlocksCompiled'].length + 1; // first calc block of rule added here , will always be 1
+                                 templateData.ruleNum = window.selectedRule;
+                                 templateData.ruleSortIndex = window.selectedRule;
+                                 templateData.calculationNum = logic.rules[window.selectedRule-1]['calculationBlocksCompiled'].length + 1; // first calc block of rule added here , will always be 1
 
                                 //Let's add a calculation to the template - from another template!
                                 var calculationBlockCompiled = HRT.templates['calculationBlock.hbs'](templateData);
 
 
-                                logic.rules[ruleNumber-1]['calculationBlocksCompiled'].push(calculationBlockCompiled);
+                                logic.rules[window.selectedRule-1]['calculationBlocksCompiled'].push(calculationBlockCompiled);
 
                                  // And finally, add the logic as a property of the selected Question
                                  window.selectedQuestion.model.set('logic', logic);
 
                                 // Notify App of calc block added.
                                 window.questionModel.set('calculationBlockAdded', true); // I only want to append a calc block
-
 
 
                                 break;
@@ -214,8 +237,8 @@ define(
                 {
 
 
-                    // Split the control name and get the rule num. The control id's must all start with rule_1 for this to work
-                    var ruleNumber = $(e.target).attr('id').split('_')[1];
+                    window.selectedRule = $(e.target).attr('id').split('_')[1];
+
                     //
                     //console.log('changed something', this.$(e.target).data('calculation-control'));
                     var selectedLogicRuleQuestionNumber;
@@ -224,6 +247,8 @@ define(
                     {
 
                         case 'operandAnswerOf':
+
+                            window.selectedCalculation = $(e.target).attr('id').split('_')[3]; // only will work if id is like rule_1_calculation_2
 
                             selectedLogicRuleQuestionNumber = this.$(e.target).find('option:selected').val();
 
@@ -234,7 +259,7 @@ define(
 
                             // But to map to it I want the 'closest' select (within this rule) with data('calculation-control') of suffixAnswerValue
 
-                            this.$('#rule_'+ruleNumber+'_suffix_answer_value > option').each(function(g,h) {
+                            this.$('#rule_'+window.selectedRule+'_suffix_answer_value > option').each(function(g,h) {
 
                                 //console.log(this.text, this.value,g,h);
                                 //console.log('loop qid ', $(this).data('question'));
